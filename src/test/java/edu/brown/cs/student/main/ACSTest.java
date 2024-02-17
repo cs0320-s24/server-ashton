72d17922179b27a94a398932fc654ede6b325129
Ashton <ashton_glover@brown.edu>
2024-02-17 16:41:50 -0500

    working on broadband testing

--- src/test/java/edu/brown/cs/student/main/ACSTest.java
+++ src/test/java/edu/brown/cs/student/main/ACSTest.java
@@ -4,7 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.brown.cs.student.main.server.handlers.broadband.BroadbandHandler;
 import edu.brown.cs.student.main.server.handlers.broadband.StateCountyInit;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Level;
@@ -34,6 +37,12 @@ public class ACSTest {
     Spark.awaitStop();
   }
 
+  /**
+   * Helper function for making a request to the API
+   * @param apiCall
+   * @return
+   * @throws IOException
+   */
   private static HttpURLConnection tryRequest(String apiCall) throws IOException {
     URL requestURL = new URL("http://localhost:" + Spark.port() + "/" + apiCall);
     HttpURLConnection clientConnection = (HttpURLConnection) requestURL.openConnection();
@@ -44,6 +53,28 @@ public class ACSTest {
     return clientConnection;
   }
 
+  /**
+   * Helper function for converting the response to a string
+   * @param connection
+   * @return
+   * @throws IOException
+   */
+  private String getResponse(HttpURLConnection connection) throws IOException {
+    BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+    StringBuilder response = new StringBuilder();
+    String inputLine;
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
+    }
+    in.close();
+
+    return response.toString();
+  }
+
+  /**
+   * Test for testing for successful connection
+   * @throws IOException
+   */
   @Test
   public void testConnection() throws IOException {
     HttpURLConnection clientConnection = tryRequest("broadband");
@@ -53,6 +84,10 @@ public class ACSTest {
     clientConnection.disconnect();
   }
 
+  /**
+   * Test for testing for unsuccessful connection
+   * @throws IOException
+   */
   @Test
   public void testFailedConnection() throws IOException {
     HttpURLConnection clientConnection = tryRequest("broad-band");
@@ -61,4 +96,6 @@ public class ACSTest {
 
     clientConnection.disconnect();
   }
+
+
 }
